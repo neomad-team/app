da91dd11c51b3a01da7282c0b7eb0e6cb9b75dff
fuentesloic <fuentes.loic@gmail.com>
2018-08-26 11:20:33 -0400

    add react native component

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Alert, Text, View } from 'react-native'
+import { Button } from 'react-native-elements'
 
 import styles from './static/style'
 import instruction from './static/instruction'
@@ -8,6 +9,12 @@ export default class App extends Component {
   render () {
     return (
       <View style={styles.container}>
+        <Button
+          title='Hello World!'
+          onPress={() => {
+            Alert.alert('You tapped the button!')
+          }}
+        />
         <Text style={styles.welcome}>Welcome there!</Text>
         <Text style={styles.instructions}>To get started edit App.js</Text>
         <Text style={styles.instructions}>{instruction}</Text>
