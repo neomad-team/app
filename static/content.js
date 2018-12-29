200162a185e1ea4841c115ea8a16e190e392ff34
fuentesloic <fuentes.loic@gmail.com>
2018-12-29 12:15:35 -0500

    fix flask redirection slash and simplify error form message

--- static/content.js
+++ static/content.js
@@ -2,12 +2,9 @@ export const content = {
   title: 'NEOMAD',
   url: 'http://localhost:5000',
   osmUrl: 'https://nominatim.openstreetmap.org',
-  imgPath: '/static/uploads/avatars',
-  loginPath: '/api/login',
-  positionPath: '/api/user/location',
+  imgPath: '/static/uploads/avatars/',
+  loginPath: '/api/login/',
+  positionPath: '/api/user/location/',
   welcome: 'Hello',
-  error: {
-    email: 'Check your email format',
-    password: 'It seems your password or email are not working'
-  }
+  error: 'It seems your password or email are not working'
 }
